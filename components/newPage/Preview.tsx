0058d348a3eb0b666ac8f794009686d31a863421
sreehari2003 <sreeharivijaya2003@gmail.com>
2022-02-24 19:09:23 +0530

    Added heading input text field

--- components/newPage/Preview.tsx
+++ components/newPage/Preview.tsx
@@ -1,24 +1,39 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
-import remarkGfm from 'remark-gfm'
+import remarkGfm from "remark-gfm";
+import TextField from "@mui/material/TextField";
+import styles from "./txt.module.scss";
 
-interface txt{
+interface txt {
   text: string;
 }
 
-const Preview = ({ text }:txt) => {
+const Preview = ({ text }: txt) => {
   return (
-    <div className="preview">
-
+    <>
       <style jsx>{`
+        .flex {
+          display: flex;
+        }
         .preview {
           width: 500px;
           border: 1px solid grey;
           height: 495px;
+          margin-top: 20px;
         }
       `}</style>
-     <ReactMarkdown remarkPlugins={[remarkGfm]}>{text}</ReactMarkdown>
-    </div>
+      <div className={styles.box}>
+        <TextField
+          id="outlined-basic"
+          label="heading"
+          variant="outlined"
+          disabled={true}
+        />
+        <div className="preview">
+          <ReactMarkdown remarkPlugins={[remarkGfm]}>{text}</ReactMarkdown>
+        </div>
+      </div>
+    </>
   );
 };
 
--- components/newPage/Text.tsx
+++ components/newPage/Text.tsx
@@ -1,24 +1,31 @@
-import React,{ useState} from 'react'
-import TextField from '@mui/material/TextField';
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import styles from "./txt.module.scss";
 
 interface txt {
-    getText:(el:string)=>void;
+  getText: (el: string) => void;
 }
-const Text = ({getText}:txt) => {
-    const [text,setText] = useState("");
-    getText(text);
-  return (
-    <div className="">
-           <TextField
-          id="outlined-multiline-static"
-          multiline
-          rows={20}
-          label="Enter your Diary"
-          value={text}
-          onChange={(e)=>setText(e.target.value)}
-        />
-    </div>
-  )
-}
+const Text = ({ getText }: txt) => {
+  const [text, setText] = useState("");
+  getText(text);
 
-export default Text
+  return (
+    <>
+      <div className={styles.box}>
+        <TextField id="outlined-basic" label="Heading" variant="outlined" />
+        <div className={styles.box_min}>
+          <TextField
+            id="outlined-multiline-static"
+            multiline
+            rows={20}
+            label="Enter your Diary"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Text;
